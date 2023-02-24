30f9eaa001b779ab4534d53e2b244b1af92b6b8d
StefieCaff <108297341+StefieCaff@users.noreply.github.com>
2023-02-24 21:22:15 +0000

    Deploying to gh-pages from @ StefieCaff/filmoteka@380ecd982e9314f50916db2c492a7df0b1d4e062 🚀

--- index.bdcea524.js
+++
@@ -1,2 +0,0 @@
-window.addEventListener("load",(async function(){const a=await fetch("https://api.themoviedb.org/3/trending/all/day?api_key=cc8aceddc1acb4be5d6024b16563f8b2"),e=await a.json();return console.log(e),e}));
-//# sourceMappingURL=index.bdcea524.js.map
