e99a31c1c4af6e72b46eee9baab8bb3e7ec4e1d2
StefieCaff <108297341+StefieCaff@users.noreply.github.com>
2023-02-27 15:48:02 -0800

    Update index.js
    search works

--- src/index.js
+++ src/index.js
@@ -12,10 +12,11 @@ const userInput = document.querySelector('input#search-query');
 const API_KEY = 'api_key=cc8aceddc1acb4be5d6024b16563f8b2';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const API_TRENDING = BASE_URL + '/trending/all/day?' + API_KEY;
-const API_SEARCH = BASE_URL + '/search/movie/?' + API_KEY;
+const API_SEARCH = BASE_URL + '/search/movie?' + API_KEY;
+const trendingParams = '&language=en-US&page=1&include_adult=false&total_pages=100'
+const searchParams = '&language=en-US&query=';
 
-
-getMovies(API_TRENDING);
+getMovies(API_TRENDING + trendingParams);
 
 // window.addEventListener("load", getMovies);
 
@@ -26,7 +27,7 @@ form.addEventListener('submit', (e) => {
     const input = userInput.value;
     if (input) {
         console.log(input)
-        getMovies(API_SEARCH + '&query=' + input);  
+        getMovies(API_SEARCH + searchParams +input + '&page=1&include_adult=false');  
     } else {
         getMovies(API_TRENDING);
     }
